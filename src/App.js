45998ac29302f31a3c7fccc4cfc016ccd408d050
Moaaz Sidat <moaaz.omer@gmail.com>
2017-09-13 12:50:53 -0700

    flow type from external node module

---
+++ src/App.js
@@ -0,0 +1,29 @@
+// @flow
+
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import { sum, SumComponent } from 'foobar-ui'
+console.log(sum, SumComponent )
+
+type Props = {}
+
+class App extends Component<Props, void> {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <SumComponent input={[1,2]}/>
+      </div>
+    );
+  }
+}
+
+export default App;
